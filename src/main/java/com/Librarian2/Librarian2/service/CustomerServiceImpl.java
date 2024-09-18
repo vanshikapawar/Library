f992524f955301975af178bf415aea36ebdcfc9a
vanshikapawar <vanshikapawar2805@gmail.com>
2024-09-18 17:16:59 +0530

    added the functionality to send mail to the customer on registeration

--- src/main/java/com/Librarian2/Librarian2/service/CustomerServiceImpl.java
+++ src/main/java/com/Librarian2/Librarian2/service/CustomerServiceImpl.java
@@ -3,6 +3,8 @@ package com.Librarian2.Librarian2.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import com.Librarian2.Librarian2.dao.CustomerDao;
@@ -14,6 +16,9 @@ public class CustomerServiceImpl implements CustomerService {
 	@Autowired
 	private CustomerDao customerDao;
 	
+	@Autowired
+    private JavaMailSender mailSender;
+	
 	@Override
 	public List<Customer> getCustomer() {
 		// TODO Auto-generated method stub
@@ -24,7 +29,22 @@ public class CustomerServiceImpl implements CustomerService {
 	public Customer addCustomer(Customer customer) {
 		// TODO Auto-generated method stub
 		customerDao.save(customer);
+		sendCustomMail(customer);
 		return customer;
 	}
+	
+	private void sendCustomMail(Customer customer) {
+        String email = customer.getEmail();  // Assuming there's an getEmail() method
+        String subject = "Welcome to Our Library";
+        String message = "Dear " + customer.getCust_name() + ",\n\n" +
+                         "Thank you for registering with our library. We hope you enjoy exploring our collection of books.\n\n" +
+                         "Best regards,\nLibrary Team";
+
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(email);
+        mailMessage.setSubject(subject);
+        mailMessage.setText(message);
+        mailSender.send(mailMessage);
+    }
 
 }
